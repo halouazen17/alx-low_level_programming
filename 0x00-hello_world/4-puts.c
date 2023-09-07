cc2a5aa48af5bc265925cb96a1838de236da064e
hicham alouazen <h.alouazen17@outlook.fr>
2023-09-07 13:03:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - entry point
  *
- * return: 0 (success)
+ * Return: 0 (success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
