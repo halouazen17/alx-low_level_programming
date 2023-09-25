2940001e39077871d0c492bb179eb672aa25c0f8
hicham alouazen <h.alouazen17@outlook.fr>
2023-09-25 14:12:52 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_chessboard - entry point
- * @a: array
+ * print_diagsums - entry point
+ * @a: input
+ * @size: input
  * Return: 0
  */
 void print_diagsums(int *a, int size)
